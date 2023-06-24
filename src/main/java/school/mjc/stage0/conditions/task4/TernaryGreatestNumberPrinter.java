3a76ab005d739174af436a8c2fcef5de0d13ad93
yuliyago <yuuliya02@proton.me>
2023-06-24 22:35:59 +0300

    Conditions were represented with the help of ternary operator.

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int max = (first>=second)?
+            first:
+            second;
+        System.out.println(max);
     }
 }
